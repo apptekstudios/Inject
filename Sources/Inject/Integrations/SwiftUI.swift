22b73a13ea592dd8a9f98dbc72474db7b7d7b798
tbren <contact@apptekstudios.com>
2022-04-12 08:43:06 +1000

    Ensure property wrapper has matching init call in release build.

--- Sources/Inject/Integrations/SwiftUI.swift
+++ Sources/Inject/Integrations/SwiftUI.swift
@@ -24,12 +24,13 @@ public extension SwiftUI.View {
 public extension SwiftUI.View {
     @propertyWrapper
     public struct Injection {
-        public private(set) var wrappedValue: Void = ()
+        public init(animation: Animation? = nil) { }
+        public private(set) var wrappedValue: Inject.Type = Inject.self
     }
     @inlinable @inline(__always)
-    func enableInjection() -> some SwiftUI.View { self }
+    func enableInjection() -> Self { self }
     @inlinable @inline(__always)
-    func enableInjection(callback: @escaping (Self) -> Void) -> some SwiftUI.View {
+    func enableInjection(callback: @escaping (Self) -> Void) -> Self {
         self
     }
 }
